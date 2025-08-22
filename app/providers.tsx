f85eaf220e37718f0effa407eb63fe27ce3a8acd
Jerald Choon <choonjerald@gmail.com>
2025-08-22 18:32:31 +0800

    Theme selector duplicate fix.

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,6 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { EnsureUser } from "@/components/ensure-user";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -17,7 +16,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
           <EnsureUser />
           {/* Global toggle (you can move this into a navbar later) */}
           <div className="fixed right-4 top-4 z-50">
-            <ModeToggle />
           </div>
           {children}
         </ConvexProviderWithClerk>
